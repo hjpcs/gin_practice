d4a1c437bda2650d75cceb2f6fc4eabfd87858b3
hejinpeng <hejinpengcs@foxmail.com>
2022-06-17 12:02:08 +0800

    add removeAlbumByID function add duplicate verify in postAlbums function

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 
 	router.POST("/albums", postAlbums)
+	router.POST("/albums/:id", removeAlbumByID)
 
 	router.Run("localhost:8080")
 }
@@ -46,6 +47,14 @@ func postAlbums(c *gin.Context) {
 		return
 	}
 
+	// verify the album exist or not
+	for _, a := range albums {
+		if a.ID == newAlbum.ID {
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "album already exist"})
+			return
+		}
+	}
+
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
@@ -60,6 +69,23 @@ func getAlbumByID(c *gin.Context) {
 	// an album whose ID value matches the parameter.
 	for _, a := range albums {
 		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// removeAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then remove that album.
+func removeAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for i, a := range albums {
+		if a.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
